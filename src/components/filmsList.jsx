23ff793ceb119965df5333bf959303d3ef710c42
Alejandro Dighiero <adighiero1@gmail.com>
2023-01-18 11:12:01 -0500

    Fixed issue

--- src/components/filmsList.jsx
+++ src/components/filmsList.jsx
@@ -10,15 +10,15 @@ class FilmsList extends Component {
   }
   getFilms() {
     fetch("https://studioghibliapi-d6fc8.web.app/films") //returns promise that needs to be consumed.
-      .then((response) => response.json()) //parses the body as a json and returns a promise that gets consumed next
+      .then((response) => response.json()) //parses the body as a json and returns a promise that gets consumed next json object
       .then((films) => {
         // setting list to films
-        this.setState({ list: films });
+        this.setState({ list: films }); //
       })
       .catch((error) => console.log(error)); //handle erros
   }
   componentDidMount() {
-    this.getFilms(); //when the component mounts, get films()
+    this.getFilms(); //when the component mounts, get films(). controls when it is rendered
   }
   render() {
     //rendering the list
