9578212610237dfbf3e7ea52beb17be7e17320c0
Alejandro Dighiero <adighiero1@gmail.com>
2023-01-23 15:09:53 -0500

    Alejandro Excercise. Converted to functional Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,41 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./App.css";
 import Edit from "./components/a";
 import Three from "./components/b";
 import FilmsList from "./components/filmsList";
-class App extends Component {
-  constructor(props) {
-    super(props); //creating constructor
-    this.state = {
-      list: ["ready", "set", "GO"], //this.state equal this object
-      text: "", // this is the text that is being changed by the onChange function
-    };
-    this.onSubmit = this.onSubmit.bind(this); //without binding on the form it doesn't refrence this. so bind it
-  }
-  onSubmit(event) {
+function App(props) {
+  let [list, setList] = useState(["ready", "set,", "go"]); //use state for the list
+  let [text, setText] = useState("");
+  function onSubmit(event) {
     //html on submit event
     event.preventDefault();
-    let newList = [...this.state.list, this.state.text]; //spread operator creates copy of previous list
-    this.setState({ list: newList, text: "" });
+    let newList = [...list, text]; //spread operator creates copy of previous list
+    setList(newList);
+    setText("");
   }
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-        <form onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            name="text"
-            id="text"
-            value={this.state.text}
-            onChange={(event) => this.setState({ text: event.target.value })} //assigning it to the value currently in the input
-          />
-          <button type="submit">Add</button>
-        </form>
 
-        <ul>
-          {this.state.list.map((item, idx) => {
-            return <li key={item + idx}>{item}</li>; //for every item i'm returning a list item
-          })}
-        </ul>
-        <FilmsList />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="text"
+          id="text"
+          value={text}
+          onChange={(event) => setText(event.target.value)} //assigning it to the value currently in the input
+        />
+        <button type="submit">Add</button>
+      </form>
+
+      <ul>
+        {list.map((item, idx) => {
+          return <li key={item + idx}>{item}</li>; //for every item i'm returning a list item
+        })}
+      </ul>
+      <FilmsList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/filmsList.jsx
+++ src/components/filmsList.jsx
@@ -1,35 +1,28 @@
-import { Component } from "react";
-
-class FilmsList extends Component {
-  constructor(props) {
-    //creating constructor to recieve props
-    super(props);
-    this.state = {
-      list: [],
-    };
-  }
-  getFilms() {
+import { useState, useEffect } from "react";
+function FilmsList(props) {
+  let [list, setList] = useState([]); //destructuring from the return hook call with initial value of empty array.
+  function getFilms() {
     fetch("https://studioghibliapi-d6fc8.web.app/films") //returns promise that needs to be consumed.
       .then((response) => response.json()) //parses the body as a json and returns a promise that gets consumed next json object
       .then((films) => {
         // setting list to films
-        this.setState({ list: films }); //
+        setList(films); //
       })
       .catch((error) => console.log(error)); //handle erros
   }
-  componentDidMount() {
-    this.getFilms(); //when the component mounts, get films(). controls when it is rendered
-  }
-  render() {
-    //rendering the list
-    return (
-      <ul>
-        {this.state.list.map((film) => {
-          return <li key={film.id}>{film.title}</li>;
-        })}
-      </ul>
-    );
-  }
+  useEffect(() => {
+    //useEffect that takes the callback function get films. empty array as dependency array to control when it renders. its blank which means it renders once.
+    getFilms();
+  }, []);
+
+  //rendering the list
+  return (
+    <ul>
+      {list.map((film) => {
+        return <li key={film.id}>{film.title}</li>;
+      })}
+    </ul>
+  );
 }
 
 export default FilmsList;
